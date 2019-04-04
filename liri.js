db2b4d4ee5feadde4864e9976e6ee66a28c25bf4
Ashell Fernando <ashellz2010@gmail.com>
2019-04-04 19:09:09 -0400

    taking in args

--- liri.js
+++ liri.js
@@ -1,2 +1,14 @@
 require('dotenv').config();
 var keys = require('./config/keys');
+var spotify = new Spotify(keys.spotify);
+
+var Args = process.argv;
+
+var searchStr = '';
+for (var i = 2; i < Args.length; i++) {
+    if (i > 2 && i < Args.length) {
+        searchStr = searchStr + '+' + Args[i];
+    } else {
+        searchStr += Args[i];
+    }
+}
