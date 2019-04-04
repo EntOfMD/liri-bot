d294df40b20915125dd256900f5561923b58666a
Ashell Fernando <ashellz2010@gmail.com>
2019-04-04 19:55:31 -0400

    concert-this work

--- liri.js
+++ liri.js
@@ -1,17 +1,44 @@
 require('dotenv').config();
-const spotify = new Spotify(keys.spotify);
-const keys = require('./config/keys');
 
+var Spotify = require('node-spotify-api');
 const axios = require('axios');
 const moment = require('moment');
+
+const keys = require('./config/keys');
+const spotify = new Spotify(keys.spotify);
 
 var Args = process.argv;
 var searchStr = '';
 
-for (var i = 2; i < Args.length; i++) {
-    if (i > 2 && i < Args.length) {
-        searchStr = searchStr + '+' + Args[i];
+for (var i = 3; i < Args.length; i++) {
+    if (i > 3 && i < Args.length) {
+        searchStr = `${searchStr} ${Args[i]}`;
     } else {
         searchStr += Args[i];
     }
+}
+console.log(Args);
+console.log(searchStr);
+function concertThis(artist) {
+    let qURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+    axios
+        .get(qURL)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            throw err;
+        });
+}
+
+switch (Args[2]) {
+    case 'concert-this':
+        concertThis(searchStr);
+        break;
+    case 'spotify-this-song':
+        break;
+    case 'movie-this':
+        break;
+    case 'do-what-it-says':
+        break;
 }
